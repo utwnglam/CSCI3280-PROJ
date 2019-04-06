a0edd662b1314f52b36dd6bf4b20866a1064c63a
PatrickkkCheng <patrick1998cheng@yahoo.com.hk>
2019-04-07 02:37:09 +0800

    first combine

--- P2Psystem/mainwindow.cpp
+++ P2Psystem/mainwindow.cpp
@@ -1,3 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#undef UNICODE
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib,"msacm32.lib")
+#include <iostream>
+#include <Windows.h>
+#include <mmsystem.h>
+#include <string>
+#include <fstream>
+#include <stdio.h>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
@@ -55,9 +65,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
         pItem->setText(tmpList[0]);
         ui->songL->addItem(pItem);
     }
-    while(!(in.atEnd()))
+    /*while(!(in.atEnd()))
     {
-        /*QString line = in.readLine();
+        QString line = in.readLine();
         QStringList strlist = line.split('\'');
 
         QListWidgetItem *pItem = new QListWidgetItem(ui->songL);
@@ -68,13 +78,13 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
         pItem->setText(strlist[3]);
         //ui->songL->addItem(pItem);
 
-        /*
+
         fileName is strlist[1], index: Qt::UserRole
         songName is strlist[3], index: Qt::UserRole + 1
         bandName is strlist[5], index: Qt::UserRole + 2
-        albumName is strlist[7], index: Qt::UserRole + 3*/
+        albumName is strlist[7], index: Qt::UserRole + 3
 
-    }
+    }*/
     file.close();
 
     /*
@@ -127,10 +137,101 @@ void MainWindow::on_Del_clicked()
 
 void MainWindow::on_playButton_clicked()
 {
+    QList<QListWidgetItem *> itemList = ui->songL->selectedItems();
+    int row= ui->songL->row(itemList[0]);
+    QString song = myList[row];
+    std::string song1=song.toStdString().c_str();
+    std::string path="C:\\Users\\user\\CSCI3280-PROJ\\P2Psystem\\Music\\"+song1+".wav";
     if(ui->playButton->text() == "play"){
         //mPlayer -> play();
+        LPSTR szFileName;
+        LPTSTR szPathName;
+        MMCKINFO mmckinfoParent;
+        MMCKINFO mmckinfoSubChunk;
+        DWORD dwFmtSize;
+        DWORD m_WaveLong;
+        WAVEFORMATEX* lpFormat;
+        DWORD m_dwDataOffset;
+        DWORD m_dwDataSize;
+        WAVEOUTCAPS pwoc;
+        LONG lSoundOffset;
+
+        LONG lSoundLong;
+
+        //szFileName = (LPTSTR)filename;
+        szPathName = (LPTSTR)path.c_str();
+        float speed = 0.8;
+        HMMIO m_hmmio;
+        printf("%s\n", szPathName);
+        if (!(m_hmmio = mmioOpen(szPathName, NULL, MMIO_READ)))
+        {
+            printf("fail in mmioOpen\n");
+        }
+
+        mmckinfoParent.fccType = mmioFOURCC('W', 'A', 'V', 'E');
+        if (mmioDescend(m_hmmio, (LPMMCKINFO)& mmckinfoParent, NULL, MMIO_FINDRIFF))
+        {
+            printf("fail in mmioDescend:WAVE\n");
+        }
+        mmckinfoSubChunk.ckid = mmioFOURCC('f', 'm', 't', ' ');
+        if (mmioDescend(m_hmmio, &mmckinfoSubChunk, &mmckinfoParent,
+            MMIO_FINDCHUNK))
+        {
+            printf("fail in mmioDescend:fmt\n");
+        }
+
+        dwFmtSize = mmckinfoSubChunk.cksize;
+        lpFormat = (WAVEFORMATEX *)LocalLock(LocalAlloc(LMEM_MOVEABLE, LOWORD(dwFmtSize)));
+        if ((unsigned long)mmioRead(m_hmmio, (HPSTR)lpFormat, dwFmtSize) != dwFmtSize)
+        {
+            printf("fail in mmioRead\n");
+        }
+        mmioAscend(m_hmmio, &mmckinfoSubChunk, 0);
+        mmckinfoSubChunk.ckid = mmioFOURCC('d', 'a', 't', 'a');
+        if (mmioDescend(m_hmmio, &mmckinfoSubChunk, &mmckinfoParent, MMIO_FINDCHUNK))
+        {
+            printf("fail in mmioDescend:data\n");
+        }
+        m_dwDataSize = mmckinfoSubChunk.cksize;
+        m_dwDataOffset = mmckinfoSubChunk.dwDataOffset;
+        if (m_dwDataSize == 0L)
+        {
+            printf("the file has no data!");
+        }
+
+        char* lpData = (char *)malloc(sizeof(char) * m_dwDataSize);
+        if (!lpData)
+        {
+            printf("Alloc memory for wave data failed!");
+        }
+
+
+        lSoundOffset = m_dwDataOffset;
+        LONG lSize = mmioSeek(m_hmmio, lSoundOffset, SEEK_SET);
+        int value;
+        if (mmioRead(m_hmmio, lpData, m_dwDataSize) != m_dwDataSize)
+        {
+            printf("something wrong in mmioRead\n");
+        }
+
+        HWAVEOUT hWaveOut;
+        lpFormat->nSamplesPerSec *= speed; // revise this variable for tuning the speed
+        waveOutOpen(&hWaveOut, WAVE_MAPPER, lpFormat, 0L, 0L, WAVE_FORMAT_DIRECT);
+
+        WAVEHDR WaveOutHdr;
+        WaveOutHdr.lpData = (LPSTR)lpData;
+        WaveOutHdr.dwBufferLength = m_dwDataSize;
+        WaveOutHdr.dwBytesRecorded = 0;
+        WaveOutHdr.dwUser = 0L;
+        WaveOutHdr.dwFlags = 0L;
+        WaveOutHdr.dwLoops = 0L;
+        waveOutPrepareHeader(hWaveOut, &WaveOutHdr, sizeof(WAVEHDR));
+        waveOutWrite(hWaveOut, &WaveOutHdr, sizeof(WAVEHDR));
+
+        do {} while (waveOutUnprepareHeader(hWaveOut, &WaveOutHdr, sizeof(WAVEHDR)) == WAVERR_STILLPLAYING);
+        waveOutClose(hWaveOut);
         ui->playButton->setText("stop");
-    } else {
+    } else{
         //mPlayer->stop();
         ui->playButton->setText("play");
     }
